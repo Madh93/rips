d818466c9b362b36b932f6ab70917ec70b503499
Madh93 <michelangelo93@gmail.com>
2015-04-04 17:13:37 +0100

    check if input file is right

--- lib/rips.rb
+++ lib/rips.rb
@@ -4,6 +4,15 @@ require "rips/error"
 
 module Rips
 
+  # Check for a valid file
+  if ARGV.empty?
+    Error::message(1)
+  elsif !File.exist? ARGV[0]
+    Error::message(2)
+  elsif !File.readable? ARGV[0]
+    Error::message(3)
+  end
+
   rips = Assembler.new
 
   File.open(ARGV[0], "r") do |f|
--- lib/rips/error.rb
+++ lib/rips/error.rb
@@ -2,7 +2,10 @@ module Rips
   module Error
 
     MESSAGES = 
-      [ "RASM ERROR (0). Unknown problem." ]
+      [ "RIPS ERROR (0). Unknown problem.",
+        "RIPS ERROR (1). No input file (try `rips file.rips`).",
+        "RIPS ERROR (2). Doesn't exist file or directory.",
+        "RIPS ERROR (3). Permission denied to read file." ]
 
     def self.message (num, *args)
       puts MESSAGES[num] % args
