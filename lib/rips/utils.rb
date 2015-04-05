6daf25f783200ea11bf1517ef8629d94f9bf1faf
Madh93 <michelangelo93@gmail.com>
2015-04-05 19:12:26 +0100

    fixed binary conversion for negative numbers

--- lib/rips/utils.rb
+++ lib/rips/utils.rb
@@ -1,6 +1,8 @@
 class Integer
   def to_bin(width)
-    num = '%0*b' % [width, self]
-    num.gsub('.','1')
+    if self < 0
+      '%0*b' % [width, (2**width-1) - ~self]
+    else
+      '%0*b' % [width, self]
   end
 end
