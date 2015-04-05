30d76eda3d1f3007d25bdd3bba8cc97bd5011e3c
Madh93 <michelangelo93@gmail.com>
2015-04-05 15:04:22 +0100

    add more methods

--- lib/rips/variables/variable.rb
+++ lib/rips/variables/variable.rb
@@ -9,6 +9,26 @@ module Rips
       def initialize (length)
         @length = length
       end
+
+      # Check if value is a register ($?)
+      def register? (value)
+        value[0] == "$"
+      end
+
+      # Check if value is a valid number (...,-1,0,1...)
+      def number? (value)
+        /\A[-]?\d+\z/ === value
+      end
+
+      # Return integer part of value
+      def to_i (value)
+        value.split('$',2).last.to_i
+      end
+
+      # Return error message about incorrent syntax
+      def error (value)
+        "unexpected `#{value}` (expected a `#{self.class.to_s.split(':').last}` argument like `#{@syntax}`)"
+      end      
     end
   end
 end
