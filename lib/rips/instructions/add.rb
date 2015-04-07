c35ed0298acc1cdb3dff80957d40a4e121db0f7f
Madh93 <michelangelo93@gmail.com>
2015-04-07 17:15:39 +0100

    fixing opcodes

--- lib/rips/instructions/add.rb
+++ lib/rips/instructions/add.rb
@@ -10,7 +10,7 @@ module Rips
       # @variables: types of instruction's variables
       # @length: length in bits for each variable
       def initialize
-        super("li",Formats::DFormat.new(0b0010))
+        super("add",Formats::DFormat.new(0b0010))
         @variables = [Variables::Register.new, Variables::Register.new, Variables::Register.new]
         @length = {r3:4, r2:4, r1:4}
       end
