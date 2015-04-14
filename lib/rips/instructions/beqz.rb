b6a442d0b97942c61e501e16ffaa67802fb4cbd1
Madh93 <michelangelo93@gmail.com>
2015-04-14 22:42:37 +0100

    change opcode

--- lib/rips/instructions/beqz.rb
+++ lib/rips/instructions/beqz.rb
@@ -10,7 +10,7 @@ module Rips
       # @variables: types of instruction's variables
       # @length: length in bits for each variable
       def initialize
-        super("bez",Formats::BFormat.new(0b001111))
+        super("beqz",Formats::BFormat.new(0b011111))
         @variables = [Variables::Address.new]
         @length = {r1:10, op:6}
       end
