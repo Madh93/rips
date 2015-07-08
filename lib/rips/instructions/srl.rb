6f2cf978c4a5f9aef85758313a6d42d6099926db
Miguel Hernández <michelangelo93@gmail.com>
2015-07-08 18:57:21 +0000

    fixing srl instruction

--- lib/rips/instructions/srl.rb
+++ lib/rips/instructions/srl.rb
@@ -10,9 +10,9 @@ module Rips
       # @variables: types of instruction's variables
       # @length: length in bits for each variable
       def initialize
-        super("srl",Formats::CFormat.new(0b0110))
-        @variables = [Variables::Register.new, Variables::Register.new, Variables::Inmediate.new]
-        @length = {r2:4, r1:4, op:4, blank:4}
+        super("srl",Formats::DFormat.new(0b0110))
+        @variables = [Variables::Register.new, Variables::Register.new, Variables::Register.new]
+        @length = {r3:4, r2:4, r1:4, op:4}
       end
     end
   end  
